ea092567adfbafa146c22cd3026d2fa6467b7ee5
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2017-06-23 19:53:05 -0700

    Updated Dropbox provider upload method to support chunked uploading of files > 150MB

--- src/CloudDrop/Providers/Dropbox/Client.php
+++ src/CloudDrop/Providers/Dropbox/Client.php
@@ -31,20 +31,14 @@ class Client implements Provider
      */
     public function upload($path, $destination = null)
     {
-        $response = $this->content()->post('files/upload', [
-            'headers' => [
-                'Content-Type' => 'application/octet-stream',
-                'Dropbox-API-Arg' => json_encode([
-                    'path' => $this->path($destination ?? basename($path)),
-                    'mode' => 'add',
-                    'autorename' => true,
-                    'mute' => false
-                ])
-            ],
-            'body' => file_get_contents($path)
-        ]);
+        $fileSize = filesize($path);
+        $destination = $this->path($destination ?? basename($path));
 
-        return json_decode($response->getBody());
+        if ($fileSize <= 150000000) {
+            return $this->simpleUpload($path, $destination);
+        }
+
+        return $this->chunkedUpload($path, $fileSize, $destination);
     }
 
     /**
@@ -164,21 +158,6 @@ class Client implements Provider
     }
 
     /**
-     * Get more contents from a directory
-     *
-     * @param string $cursor Current cursor supplied by list_folder request
-     *
-     * @return object Folder listing object
-     */
-    protected function listContinue($cursor) {
-        $response = $this->api()->post('files/list_folder/continue', [
-            'json' => ['cursor' => $cursor]
-        ]);
-
-        return json_decode($response->getBody());
-    }
-
-    /**
      * Get the Dropbox API RPC endpoint client
      *
      * @return GuzzleHttp\Client GuzzleHttp client instance
@@ -206,6 +185,141 @@ class Client implements Provider
                 'Authorization' => 'Bearer ' . $this->accessToken
             ]
         ]);
+    }
+
+    /**
+     * Upload a file less than or equal to 150MB in a single transaction.
+     *
+     * @param string $path Path to file to be uploaded
+     * @param string $destination Path/name of resulting file in Dropbox
+     *
+     * @return object Uploaded file metadata object
+     */
+    protected function simpleUpload($path, $destination)
+    {
+        $response = $this->content()->post('files/upload', [
+            'headers' => [
+                'Content-Type' => 'application/octet-stream',
+                'Dropbox-API-Arg' => json_encode([
+                    'path' => $destination,
+                    'mode' => 'add',
+                    'autorename' => true,
+                    'mute' => false
+                ])
+            ],
+            'body' => file_get_contents($path)
+        ]);
+
+        return json_decode($response->getBody());
+    }
+
+    /**
+     * Upload a file in multiple chunks. Required for files over 150MB in size.
+     *
+     * @param string $path Path to file to be uploaded
+     * @param int $fileSize Size of file in bytes
+     * @param string $destination Path/name of resulting file in Dropbox
+     *
+     * @return object Upload session object
+     */
+    protected function chunkedUpload($path, $fileSize, $destination)
+    {
+        $upload = $this->uploadStart();
+
+        for ($offset = 0; $offset <= $fileSize; $offset += 150000000) {
+            $chunk = file_get_contents($path, false, null, $offset, 150000000);
+            $this->uploadAppend($upload->session_id, $chunk, $offset);
+        }
+
+        return $this->uploadFinish($upload->session_id, $fileSize, $destination);
+    }
+
+    /**
+     * Start a chunked upload session.
+     *
+     * @return object Upload session object
+     */
+    protected function uploadStart()
+    {
+        $response = $this->content()->post('files/upload_session/start', [
+            'headers' => [
+                'Content-Type' => 'application/octet-stream'
+            ]
+        ]);
+
+        return json_decode($response->getBody()->getContents());
+    }
+
+    /**
+     * Upload a chunk of data belonging to an upload session.
+     *
+     * @param string $sessionId Upload session ID
+     * @param string $chunk String of chunk data, should not exceed 150MB
+     * @param int $offset Offset for the chunk being appended
+     *
+     * @return object Upload session object
+     */
+    protected function uploadAppend($sessionId, $chunk, $offset)
+    {
+        $this->content()->post('files/upload_session/append_v2', [
+            'headers' => [
+                'Content-Type' => 'application/octet-stream',
+                'Dropbox-API-Arg' => json_encode([
+                    'cursor' => [
+                        'session_id' => $sessionId,
+                        'offset' => $offset
+                    ]
+                ])
+            ],
+            'body' => $chunk
+        ]);
+    }
+
+    /**
+     * Close a chuncked upload session.
+     *
+     * @param string $sessionId Upload session ID
+     * @param int $fileSize Size of file in bytes
+     * @param string $destination Path/name of resulting file in Dropbox
+     *
+     * @return object Uploaded file metadata object
+     */
+    protected function uploadFinish($sessionId, $fileSize, $destination)
+    {
+        $response = $this->content()->post('files/upload_session/finish', [
+            'headers' => [
+                'Content-Type' => 'application/octet-stream',
+                'Dropbox-API-Arg' => json_encode([
+                    'cursor' => [
+                        'session_id' => $sessionId,
+                        'offset' => $fileSize
+                    ],
+                    'commit' => [
+                        'path' => $destination,
+                        'mode' => 'add',
+                        'autorename' => false,
+                        'mute' => false
+                    ]
+                ])
+            ]
+        ]);
+
+        return json_decode($response->getBody());
+    }
+
+    /**
+     * Get more contents from a directory
+     *
+     * @param string $cursor Current cursor supplied by list_folder request
+     *
+     * @return object Folder listing object
+     */
+    protected function listContinue($cursor) {
+        $response = $this->api()->post('files/list_folder/continue', [
+            'json' => ['cursor' => $cursor]
+        ]);
+
+        return json_decode($response->getBody());
     }
 
     /**
